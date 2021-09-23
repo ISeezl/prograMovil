6f5da8c3317fae09b8597cc60a5776962b41902b
ISeezl <s.lopezmunoz@duocuc.cl>
2021-09-23 12:37:00 -0300

    v0.2 cambio paginas

--- src/app/inicio-sesion/inicio-sesion.page.ts
+++ src/app/inicio-sesion/inicio-sesion.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioSesionPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
+
+  goToMenu(){
+    this.router.navigate(['/menu'])
+
+  }
+
+ 
 
   ngOnInit() {
   }
 
+
 }
+
