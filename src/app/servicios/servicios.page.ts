534d565188a8b70ae187c6ec754e2da82cd04e3b
Manux03 <ma.ariast@duocuc.cl>
2021-09-28 11:04:31 -0300

    Pagina de servicios y Childrens
    Se agregan las paginas de servicios, agendar hora y Childrens

---
+++ src/app/servicios/servicios.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-servicios',
+  templateUrl: './servicios.page.html',
+  styleUrls: ['./servicios.page.scss'],
+})
+export class ServiciosPage implements OnInit {
+
+  constructor(private router:Router) { }
+
+  goToAgendarHora(){
+    this.router.navigate(['/servicios/reserva-hora'])
+
+  }
+
+  ngOnInit() {
+  }
+
+}
